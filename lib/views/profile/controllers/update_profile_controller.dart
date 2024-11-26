17a3ebcc0fc9e519ccdd5599bdb3bf4cb8f99eed
pupha2546 <pupha.2003@mail.kmutt.ac.th>
2024-11-26 20:59:39 +0700

    MBEA-82 Fix update image profile. (file type)

--- lib/views/profile/controllers/update_profile_controller.dart
+++ lib/views/profile/controllers/update_profile_controller.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http_parser/http_parser.dart';
+
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 import 'package:mbea_ssi3_front/controller/token_controller.dart';
@@ -12,6 +14,9 @@ class UpdateProfileController extends GetxController {
 
   Future<void> updateProfile(UpdateProfileRequest profileData) async {
     try {
+      print(profileData.imageUrl);
+      print(
+          "This is data before updating profile: ${userProfileController.fetchUserProfile()}");
       // Get the token for Authorization
       final token = Get.find<TokenController>().accessToken.value;
       final Map<String, String> formData = {
@@ -38,20 +43,29 @@ class UpdateProfileController extends GetxController {
           request.fields[key] = value;
         });
 
+        String mimeType =
+            profileData.imageUrl!.endsWith('.png') ? 'png' : 'jpg';
         // Add the image file to the request
-        final imageFile = await http.MultipartFile.fromPath('image', profileData.imageUrl!);
+        final imageFile = await http.MultipartFile.fromPath(
+            'image_file', profileData.imageUrl!,
+            contentType: MediaType('image', mimeType));
         request.files.add(imageFile);
 
         // Send the request
         final response = await request.send();
+        final responseData = await response.stream.bytesToString();
 
         // Check the response status
         if (response.statusCode == 200) {
           Get.snackbar('Success', 'Profile updated successfully.');
           print("user image not null");
-          print("This is data after updating profile: ${userProfileController.fetchUserProfile()}");
+          print(
+              "This is data after updating profile: ${userProfileController.fetchUserProfile()}");
           print("userProfile value: ${profileData}");
         } else {
+          print(
+              '-----------------------------------------------------------------');
+          print('Response: $responseData');
           Get.snackbar('Error', 'Failed to update profile.');
         }
       } else {
@@ -79,4 +93,3 @@ class UpdateProfileController extends GetxController {
     }
   }
 }
-
